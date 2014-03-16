f1ba5bd731e9bc4dbff20b081ae5af08469c47db
Kenzie Togami <ket1999@gmail.com>
2014-03-16 16:02:53 -0700

    App loads async properly

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
 // app.js
 // OnlineiTunes
+var OI = {
+    $app: jQuery('#app-div'),
+    load:
+        function() {
+            console.log("Loading app...");
+        }
+};
+jQuery(document).ready(OI.load);

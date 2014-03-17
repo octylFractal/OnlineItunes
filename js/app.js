c9ba9550d7a6bc264d241a599490b882f2436611
Kenzie Togami <ket1999@gmail.com>
2014-03-16 21:43:30 -0700

    This looks better

--- js/app.js
+++ js/app.js
@@ -6,9 +6,8 @@ var OI = {
         function($) {
             console.log("Loading app...");
             $app = this.$app;
-            console.log($().alert);
             $app.append($.parseHTML(
-                '<div class="alert alert-dismissable alert-warning">' +
+                '<div class="alert alert-dismissable alert-info">' +
                 '<button type="button" class="close" data-dismiss="alert">' +
                 '&times;' +
                 '</button>' +

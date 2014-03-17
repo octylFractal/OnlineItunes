fd3fa7cf7c1e87ece7f21c577423ecf9de08c338
Kenzie Togami <ket1999@gmail.com>
2014-03-17 16:23:02 -0700

    Adding file loading

--- js/app.js
+++ js/app.js
@@ -6,14 +6,19 @@ var OI = {
         function($) {
             console.log("Loading app...");
             $app = this.$app;
-            $app.append($.parseHTML(
-                '<div class="alert alert-dismissable alert-info">' +
-                '<button type="button" class="close" data-dismiss="alert">' +
-                '&times;' +
-                '</button>' +
-                'Writing Code...' +
+            var dzone = $.parseHTML(
+                '<div id="dzone" class="alert alert-dismissable alert-info">' +
+                'Drop Your iTunes XML File Here' +
                 '</div>'
-            ));
+            );
+            $app.append(dzone);
+            var $dzone = $(dzone);
+            $dzone.on('dragover', this.FILE.dragEvent);
+            $dzone.one('drop', this.FILE.selectEvent);
+            this.FILE.setCallback(function(file) {
+                $dzone.off('dragover');
+                $dzone.text("Loading '" + file.name + "'");
+            });
         },
     XML:
         (function($) {
@@ -27,18 +32,46 @@ var OI = {
                     }
                 }
             };
-            var XMLS = new XMLSerializer();
+            obj.XMLS = new XMLSerializer();
             obj.encode = function(jxml) {
                 // we assume we were passed the result of decode or something similar
                 jxml = jxml[0];
                 if (jxml && typeof jxml === 'object') {
-                    if (XMLS) {
-                        return XMLS.serializeToString(jxml);
+                    if (obj.XMLS) {
+                        return obj.XMLS.serializeToString(jxml);
                     } else if (xml in jxml) {
                         return jxml.xml;
                     } else {
                         return undefined;
                     }
+                }
+            }
+            return obj;
+        })(window.jQuery),
+    FILE:
+        (function($) {
+            var obj = {};
+            obj.selectEvent = function(evt) {
+                evt.stopPropagation();
+                evt.preventDefault();
+                var files = evt.originalEvent.dataTransfer.files; // FileList object.
+                var file = files[0]; // choose the first one
+                obj.lastFile = file;
+                obj.callback.call(window, obj.lastFile);
+            };
+            obj.dragEvent = function(evt) {
+                evt.stopPropagation();
+                evt.preventDefault();
+                evt.originalEvent.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+            };
+            obj.lastFile = undefined;
+            obj.current = function() {
+                return obj.lastFile;
+            }
+            obj.callback = $.noop();
+            obj.setCallback = function(callback) {
+                if (callback && typeof callback === 'function') {
+                    obj.callback = callback;
                 }
             }
             return obj;

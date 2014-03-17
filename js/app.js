ca4c9f2feda903cc8553d47878e8c22b3e9c4410
Kenzie Togami <ket1999@gmail.com>
2014-03-16 22:01:08 -0700

    XML helper

--- js/app.js
+++ js/app.js
@@ -14,6 +14,35 @@ var OI = {
                 'Writing Code...' +
                 '</div>'
             ));
-        }
+        },
+    XML:
+        (function($) {
+            var obj = {};
+            obj.decode = function(xml) {
+                if (xml && typeof xml === 'string') {
+                    try {
+                        return $($.parseXML(xml));
+                    } catch (err) {
+                        return undefined;
+                    }
+                }
+            };
+            var XMLS = new XMLSerializer();
+            obj.encode = function(jxml) {
+                // we assume we were passed the result of decode or something similar
+                jxml = jxml[0];
+                if (jxml && typeof jxml === 'object') {
+                    if (XMLS) {
+                        return XMLS.serializeToString(jxml);
+                    } else if (xml in jxml) {
+                        return jxml.xml;
+                    } else {
+                        return undefined;
+                    }
+                }
+            }
+            return obj;
+        })(window.jQuery)
 };
 jQuery(document).ready(jQuery.proxy(OI.load, OI));
+console.log(OI.XML.encode(OI.XML.decode('<xml>LOL_XML</xml>')));
